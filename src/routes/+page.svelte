8380a5bde09d875a2042ccc8902fc167a3aca39b
Raghad <raghad11ali@gmail.com>
2024-06-21 22:13:57 +0300

    Raghad 1pro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  
+  <svelte:head>
+    <title>
+      calculator
+    </title>
+  </svelte:head>  
+  <div class="bg-white h-[30rem] w-[20rem] rounded-3xl flex 
+  justify-center items-start pt-2">
+    <div class=" bg-[#2088fe] h-[3rem] w-[19rem]  rounded-3xl ">
+    </div>
+  </div>
